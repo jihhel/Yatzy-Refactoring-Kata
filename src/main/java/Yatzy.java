f8572d820881dbd140793d0eac3613160ae6ab1c
Jonathan Lecointe <jonathan.lecointe@link-value.fr>
2022-05-01 21:47:49 +0200

    step2 bis

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -23,48 +23,16 @@ public class Yatzy {
     public static int threes(Roll h) {
         return nbOccurrencesInRoll(h, 3);
     }
-
-
-    protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
-    {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+    public static int fours(Roll r) {
+        return nbOccurrencesInRoll(r, 4);
     }
 
-    public int fours()
-    {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+    public static int fives(Roll r) {
+        return nbOccurrencesInRoll(r, 5);
     }
 
-    public int fives()
-    {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
-    }
-
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+    public static int sixes(Roll r) {
+        return nbOccurrencesInRoll(r, 6);
     }
 
     public static int score_pair(int d1, int d2, int d3, int d4, int d5)
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -41,23 +41,23 @@ public class YatzyTest {
     @Test
     public void fours_test() 
     {
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
+        assertEquals(12, Yatzy.fours(new Roll(4,4,4,5,5)));
+        assertEquals(8, Yatzy.fours(new Roll(4,4,5,5,5)));
+        assertEquals(4, Yatzy.fours(new Roll(4,5,5,5,5)));
     }
 
     @Test
     public void fives() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
+        assertEquals(10, Yatzy.fives(new Roll(4,4,4,5,5)));
+        assertEquals(15, Yatzy.fives(new Roll(4,4,5,5,5)));
+        assertEquals(20, Yatzy.fives(new Roll(4,5,5,5,5)));
     }
 
     @Test
     public void sixes_test() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
+        assertEquals(0, Yatzy.sixes(new Roll(4,4,4,5,5)));
+        assertEquals(6, Yatzy.sixes(new Roll(4,4,6,5,5)));
+        assertEquals(18, Yatzy.sixes(new Roll(6,5,6,6,5)));
     }
 
     @Test
